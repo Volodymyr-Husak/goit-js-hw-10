ccbcb92461a04108fc71881fed1a9cd4660dcc70
Volodymyr Husak <volodyaog2907@gmail.com>
2022-09-08 01:14:55 +0300

    corrects task (2)

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,12 @@
 export default function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages` //
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+    // `https://restcountries.com/v2/name/{name}`
   )
     .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
       return response.json();
     })
     .catch(error => {
--- src/index.js
+++ src/index.js
@@ -14,81 +14,98 @@ const refs = {
 };
 const { inputEl, countryListEl, countryInfoEl } = refs;
 
-inputEl.addEventListener('input', debounce(onInput, 300));
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
   let inputValue = event.target.value.trim();
   if (inputValue === '') {
     return;
   }
-  // console.log(event.target.value);
-  // console.log(event.target.value.trim());
 
   const country = inputValue;
-  // country.trim();
 
   fetchCountries(country)
     .then(country => {
-      console.log('Країни в then:', country);
+      // console.log(country);
       renderCountryMarkup(country);
-
-      //   return country;
     })
     .catch(error => {
       // console.warn(error);
       Notiflix.Notify.failure('Oops, there is no country with that name.');
     });
 }
-let itemCountryEl = '';
-let countryInfoItemEl = '';
 
 function renderCountryMarkup(country) {
-  if (country.length > 10) {
-    itemCountryEl = document.querySelectorAll('.item-country');
-    itemCountryEl.forEach(item => item.remove());
+  let itemCountryEl = document.querySelectorAll('.item-country');
+  let countryInfoItemEl = document.querySelector('.country-info-item');
 
+  if (country.length > 10) {
+    itemCountryEl?.forEach(item => item.remove());
+    // if (countryInfoItemEl) {
+    countryInfoItemEl?.remove();
+    // }
     return Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
-  } else if (country.length === 1) {
-    countryInfoItemEl = document.querySelector('.country-info-item');
+  }
+  if (country.length === 0) {
+    itemCountryEl?.forEach(item => item.remove());
+    countryInfoItemEl?.remove();
+  }
+
+  if (country.length === 1) {
+    // countryInfoItemEl = document.querySelector('.country-info-item');
     if (countryInfoItemEl) {
       return;
     }
 
-    itemCountryEl = document.querySelectorAll('.item-country');
     itemCountryEl.forEach(item => item.remove());
 
-    const markupInfo = country.map(
-      count => `<div class="country-info-item">
-      <div class="country-flag-container">
-          <img class="country-flags" src="${count.flags.svg}"
-               alt="flags ${count.name.common}" width=100 >
-          <p>${count.name.common}</p>
-    </div>
-    
-    <ul>
-        <li>Capital: ${count.capital}</li>
-        <li>Population: ${count.population}</li>
-        <li>Languages: ${count.languages}</li>
-    </ul>
-    </div>`
+    const countryObj = country[0];
+
+    // const countryLanguagesArr = Object.values(countryObj.languages);
+    let countryLanguagesArr = [];
+
+    const countryLanguagesArrObj = countryObj.languages;
+
+    countryLanguagesArrObj.map(countryLanguage =>
+      countryLanguagesArr.push(countryLanguage.name)
     );
+
+    const markupInfo = `<div class="country-info-item">
+       <div class="country-flag-container">
+           <img class="country-info__flags" src="${countryObj.flags.svg}"
+               alt="flags ${countryObj.name}" width=30 >
+          <p class="country-info__text">${countryObj.name}</p>
+       </div>
+    
+     <ul class="country-info__list">
+         <li class="country-info__item"><span class="country-info__bold-text">Capital:</span> ${countryObj.capital}</li>
+         <li class="country-info__item"><span class="country-info__bold-text">Population:</span> ${countryObj.population}</li>
+         <li class="country-info__item"><span class="country-info__bold-text">Languages:</span> ${countryLanguagesArr}</li>
+     </ul>
+     </div>`;
+
     return countryInfoEl.insertAdjacentHTML('beforeend', markupInfo);
   }
-  countryInfoItemEl = document.querySelector('.country-info-item');
-  if (countryInfoItemEl) {
-    countryInfoItemEl.remove();
-  }
 
-  const markup = country.map(
-    count =>
-      `<li class="item-country">
+  // if (countryInfoItemEl) {
+  countryInfoItemEl?.remove();
+  // }
+  // if (itemCountryEl) {
+  itemCountryEl?.forEach(item => item.remove());
+  // }
+
+  const markupList = country
+    .map(
+      count =>
+        `<li class="item-country">
             <img class="country-flag" src="${count.flags.svg}"
-               alt="flags ${count.name.common}" width=30>
-            <p>${count.name.common}</p>
+               alt="flags ${count.name}" width=30>
+            <p class="country-list__text">${count.name}</p>
       </li>`
-  );
+    )
+    .join('');
 
-  countryListEl.insertAdjacentHTML('beforeend', markup);
+  countryListEl.insertAdjacentHTML('beforeend', markupList);
 }
